151456014c75cf2bbb6500c46d783a0622ec7d97
bchen03 <bchen03@yahoo.com>
2018-06-10 20:13:32 -0400

    Added spinners

--- modal.js
+++ modal.js
@@ -1,12 +1,20 @@
 const clickBtn = document.getElementById("clickBtn")
 const modal = document.getElementsByClassName('modal')[0]
 const closeBtn = document.getElementsByClassName('closeBtn')[0]
+const main = document.getElementsByClassName('main')[0]
 
 clickBtn.addEventListener('click', handleClick)
 closeBtn.addEventListener('click', handleClose)
 
 function handleClick(evt) {
-    modal.style.display = "block"
+    modal.style.display = "none"
+    main.classList.add("spinner");
+    
+    setTimeout(() => {
+        main.classList.remove("spinner");
+        modal.style.display = "block"
+    }, 3000);
+
 }
 
 function handleClose(evt) {
